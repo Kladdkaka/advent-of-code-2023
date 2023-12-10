770e9429542cb05f07ffbc4ce394ee154f64202d
Erik Wheeler <erik@erikssonwheeler.se>
2023-12-10 10:04:42 +0100

    solved day 1 part 2

--- 1/solution.py
+++ 1/solution.py
@@ -1,9 +1,33 @@
 from pathlib import Path
-from string import digits
+import string
+from typing import Tuple
+from operator import itemgetter
+
+# zero is not used, just to make index correct 
+digit_names = ['_', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+digits = [*string.digits]
+
+digit_pairs = list(enumerate(digit_names)) + list(enumerate(digits))
+
+print(digit_pairs)
+
+# make this not greedy later
+def find_digit(s: str) -> Tuple[int, int]: 
+  indexes = []
+
+  for num, digit in digit_pairs:
+    for i in range(0, len(s) - len(digit) + 1):
+      sub = s[i:i + len(digit)]
+      if digit == sub:
+        indexes.append((i, num))
+
+  return indexes
 
 def parse_calibration_value(s: str) -> int:
-  first_digit = next(c for c in s if c in digits)
-  last_digit = next(c for c in s[::-1] if c in digits)
+  indexes = find_digit(s)
+
+  _, first_digit = min(indexes, key=itemgetter(0))
+  _, last_digit = max(indexes, key=itemgetter(0))
 
   assert not (first_digit is None or last_digit is None)
   
